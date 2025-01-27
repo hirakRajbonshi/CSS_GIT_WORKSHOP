24089875e7ce09af51c3a34d3c4add6b1f272427
Bhumisti2006 <bhumisti_ug_24@cse.nits.ac.in>
2025-01-27 18:38:25 +0530

    Fixed Error

--- p2.c
+++ p2.c
@@ -5,7 +5,7 @@ int main() {
     int n;
     scanf("%d", &n);
     int sum = 0;
-    for(int i = 1; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         sum += i;
     }
     printf("Sum of numbers from 1 to %d is %d\n", n, sum);
